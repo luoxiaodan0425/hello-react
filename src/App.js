dffb9b53ee2d58a9ea4a657745883f063327e019
luoxiaodan0425 <532526716@qq.com>
2018-06-21 00:43:15 +0800

    第二次提交

--- src/App.js
+++ src/App.js
@@ -1,16 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Header from './components/Header';
+import Home from './components/Home';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      homeLink: "Home"
+    }
+  }
+  onGreet(age) {
+    alert(age);
+  }
+  onChangeLinkName(newName) {
+    this.setState({
+      homeLink: newName
+    })
+  }
   render() {
+    const user = {
+      name: "Anna",
+      hobbies: ["Sports", "Reading"]
+    }
     return (
       <div className="container">
         <div className="row">
-          <div className="col-xs-l col-xs-offset-11">
-            <h1>hello!</h1>
+          <div className="col-xs-1 col-xs-offset-11">
+            <Header homeLink={this.state.homeLink} />
           </div>
-        </div> 
+        </div>
+        <div className="row">
+          <div className="col-xs-1 col-xs-offset-11">
+            <Header />
+            {2+2}
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-1 col-xs-offset-11">
+            <h1>Hello !!</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-1 col-xs-offset-11">
+          <Home
+              name={"Max"}
+              initialAge={12}
+              user={user}
+              greet={this.onGreet}
+              changeLink={this.onChangeLinkName.bind(this)}
+              initialName={this.state.homeLink}
+            >
+              <p>I am child</p>
+            </Home>
+          </div>
+        </div>
       </div>
     );
   }
